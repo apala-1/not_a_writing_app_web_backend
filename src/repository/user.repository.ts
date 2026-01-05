76544e12f259dfa90249699fd3949f9dd34c2773
apala-1 <apalalamichhane13579@gmail.com>
2026-01-05 10:38:14 +0545

    created more functions in repository

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -5,8 +5,34 @@ export interface IUserRepository {
 }
 
 export class UserRepository implements IUserRepository {
+    async getUserById(id: string): Promise<IUser | null> {
+        // UserModel.findOne( {"_id": id} )
+        const user = await UserModel.findById(id);
+        return user;
+    }
+    async getAllUsers(): Promise<IUser[]> {
+        const users = await UserModel.find();
+        return users;
+    }
+    async updateUser(id: string, updateData: Partial<IUser>): Promise<IUser | null> {
+        // UserModel.updateOne( {"_id": id}, { $set: updateData } )
+        const user = await UserModel.findByIdAndUpdate(id, updateData, { new: true });
+        return user;
+    }
+    async deleteUser(id: string): Promise<boolean> {
+        const result = await UserModel.findByIdAndDelete(id);
+        return result ? true : false;
+    }
     async createUser(userData: Partial<IUser>): Promise<IUser> {
         const user = new UserModel(userData);
         return await user.save();
+    }
+    async getUserByEmail(email: string): Promise<IUser | null> {
+        const user = await UserModel.findOne({ "email": email });
+        return user;
+    }
+    async getUserByUsername(username: string): Promise<IUser | null> {
+        const user = await UserModel.findOne({ "username": username });
+        return user;
     }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,3 +3,15 @@ import { UserRepository } from "../repository/user.repository";
 import { HttpError } from "../errors/http-error";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+
+let userRepository = new UserRepository();
+
+export class UserService {
+    async createUser(data: CreateUserDTO) {
+        const emailCheck = await userRepository.getUserByEmail(data.email);
+        if(emailCheck){
+            throw new HttpError("Email already in use", 403);
+        }
+    }
+}
