192711d9d3eea44af7b68a2022cf560ead51689c
apala-1 <apalalamichhane13579@gmail.com>
2026-01-05 10:45:02 +0545

    created the index main file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import authRoutes from './routes/auth.route';
+import dotenv from 'dotenv';
 import { PORT } from './config';
 import { connectToDatabase } from './database/mondodb';
 
-const app:Application = express();
+dotenv.config();
+console.log(process.env.PORT);
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
 
 async function startServer() {
-        await connectToDatabase();
-        app.listen(PORT, () => {
-            console.log(`Server is running at http://localhost:${PORT}`);
-        });
+    await connectToDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
 }
 
 startServer();
