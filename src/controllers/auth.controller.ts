9c149b6b4527bf9f619ea96c0d22a6eff0a27f8c
apala-1 <apalalamichhane13579@gmail.com>
2026-01-20 05:59:36 +0545

    all errors fixed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,26 +6,9 @@ import z, { success } from "zod";
 let userService = new UserService();
 
 export class AuthController {
-    async logout(req: Request, res: Response) {
-    try {
-        res.clearCookie("token", {
-            httpOnly: true,
-            sameSite: "lax",
-        });
-
-        res.clearCookie("user_data");
-
-        return res.status(200).json({
-            success: true,
-            message: "Logged out successfully",
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Logout failed",
-        });
+    async createUser(req: Request, res: Response) {
+        // can eb same as AuthController.register
     }
-}
 
     async register(req: Request, res: Response) {
         try {
@@ -61,24 +44,8 @@ export class AuthController {
             }
             const loginData: LoginUserDTO = parsedData.data;
             const { token, user } = await userService.loginUser(loginData);
-
-            // setting cookie
-            res.cookie("token", token, {
-                httpOnly: true,     // JS cannot access it
-                secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
-                maxAge: 24 * 60 * 60 * 1000, // 1 day
-            });
-
-            // removing sensitive fields
-            const safeUser = {
-                id: user._id,
-                email: user.email,
-                role: user.role ?? "user",
-            };
-
             return res.status(200).json(
-                { success: true, message: "Login Successful", data: { user: safeUser } }
+                { success: true, message: "Login Successful", data: { user, token } }
             );
         } catch (error: Error | any) { // exception handling
             return res.status(error.statusCode ?? 500).json(
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.route';
 import dotenv from 'dotenv';
-import cors from "cors";
 import { PORT } from './config';
 import { connectToDatabase } from './database/mondodb';
 
@@ -13,11 +11,6 @@ console.log(process.env.PORT);
 const app: Application = express();
 
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-}));
 
 app.use('/api/auth', authRoutes);
 
