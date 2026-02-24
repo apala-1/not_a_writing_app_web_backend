956e5a3ea16028610067bbeebe976acdab76d268
apala-1 <apalalamichhane13579@gmail.com>
2026-02-24 16:38:48 +0545

    Fetching user details and own posts from backend

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -7,6 +7,24 @@ const router = Router();
 
 router.use(authorizedMiddleware);
 
+import { Request, Response, NextFunction } from "express";
+
+router.get(
+  "/me",
+  (req: Request<{ userId: string }>, res: Response) => {
+    req.params.userId = req.user!._id.toString();
+    return profileController.getWholeProfile(req, res);
+  }
+);
+
+router.get(
+  "/me/posts",
+  (req: Request<{ userId: string }>, res: Response) => {
+    req.params.userId = req.user!._id.toString();
+    return profileController.getOwnPosts(req, res);
+  }
+);
+
 router.get("/profile/:userId", profileController.getWholeProfile.bind(profileController));
 // GET /profile/:userId/posts
 router.get("/profile/:userId/posts", profileController.getOwnPosts.bind(profileController));
@@ -14,5 +32,6 @@ router.get("/:userId", profileController.getProfile.bind(profileController));
 router.post("/follow", profileController.follow.bind(profileController));
 router.post("/unfollow", profileController.unfollow.bind(profileController));
 router.post("/post-action", profileController.addPostAction.bind(profileController));
+
 
 export default router;
